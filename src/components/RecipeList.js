1b8c8048b8b99643a7987e57ce3e7024d24b1eb7
AnnuitasTechDev <calebolojo@annuitas.com>
2019-05-21 14:28:51 -0400

    The complete build

---
+++ src/components/RecipeList.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Recipe from './Recipe'
+import RecipeSearch from './RecipeSearch'
+
+export class RecipeList extends Component {
+
+  render() {
+
+    const {recipes,
+         handleDetails, 
+         value, 
+         handleSubmit, 
+         handleChange,
+         error
+        } = this.props
+    
+
+    return (
+      <React.Fragment>
+
+        <RecipeSearch 
+        value={value} 
+        handleChange={handleChange}
+         handleSubmit={handleSubmit}/>
+
+
+            <div className="container my-5">
+            {/* title */}
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+                        <h1 className="text-slanted"> Recipe List</h1>
+                    </div>
+                </div>
+                {/* END of title */}
+
+                <div className="row">
+                { error ? <h1 className="text-danger text-cnter">{error}</h1> : 
+                        recipes.map((recipe) => {
+                            return(
+                                <Recipe 
+                                key={recipe.recipe_id}
+                                recipe={recipe}
+                                handleDetails={handleDetails}
+                                />
+                            )
+                        })
+                }
+
+                  
+                </div>
+            </div>
+       
+      </React.Fragment>
+    )
+  }
+}
+
+export default RecipeList
